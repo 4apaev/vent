ae8436ed48ad011bf365863f86f0c7364d949765
michael <4apaev@gmail.com>
2016-05-07 14:37:14 +0300

    extend provided object rather than create new one

--- test.js
+++ test.js
@@ -1,110 +1,96 @@
 'use strict';
 
-'use strict';
 let log = console.log.bind(console)
 let assert = require('assert')
-let Emitter = require('./vent')
-let slice = [].slice.call.bind([].slice)
+let vent = require('./vent')
 
 let noop = assert.fail.bind(assert, 1, 0, 'must not be invoked')
 let yep = cb => cb()
 
-function run(method, x) {
-  let args = slice(arguments, 2)
+function run(method, x, ...args) {
   return function() {
-    try { x[method].apply(x, args) }
-    catch(e) { assert.fail(0, e, e.toString()) }}}
+    try {
+      x[method].apply(x, args)
+    } catch(e) {
+      assert.fail(0, e, e.toString())
+    }
+  }
+}
 
-describe('constructor', () => {
-  let ev = new Emitter;
-
-  it('should return vents object',   () => assert.equal('object',   typeof ev))
-  it('emit should be an function',   () => assert.equal('function', typeof ev.emit))
-  it('off should be an function',    () => assert.equal('function', typeof ev.off))
-  it('on should be an function',     () => assert.equal('function', typeof ev.on))
-  it('on should be once function',   () => assert.equal('function', typeof ev.once))
+describe('Vent:constructor', () => {
+  let ev = vent({});
+  it('should return vents object',   () => assert.equal('object',   typeof ev));
+  it('emit should be an function',   () => assert.equal('function', typeof ev.emit));
+  it('off should be an function',    () => assert.equal('function', typeof ev.off));
+  it('on should be an function',     () => assert.equal('function', typeof ev.on));
+  it('on should be once function',   () => assert.equal('function', typeof ev.once));
 })
 
-describe('on', () => {
-  it('should not throw when called with a string and a function', run('on', new Emitter, 'ping', log))
-  it('should not throw when called with a string, a function and an object', run('on', new Emitter, 'ping', log, {}))
+describe('Vent:on', () => {
+  it('should not throw when called with a string and a function', run('on', vent({}), 'ping', log))
+  it('should not throw when called with a string, a function and an object', run('on', vent({}), 'ping', log, {}))
 })
 
-describe('off', () => {
-  let ev = new Emitter
+describe('Vent:off', () => {
+  let ev = vent({});
   ev.on('ping', log);
-  it('should not throw when called with not registered event',    run('off', ev, 'kong'))
-  it('should not throw when called with not registered function', run('off', ev, 'ping', noop))
-  it('should not throw when called with a string and a function', run('off', ev, 'ping', log))
+  it('should not throw when called with not registered event',    run('off', ev, 'kong'));
+  it('should not throw when called with not registered function', run('off', ev, 'ping', noop));
+  it('should not throw when called with a string and a function', run('off', ev, 'ping', log));
 })
 
-describe('emit', () => {
-  let ev = new Emitter
-  beforeEach(() => ev.off())
+describe('Vent:emit', () => {
+  let ev = vent({})
+  beforeEach(() => ev.off());
 
-  it('should emit ping event and invoke callback', function(done) {
-    ev.on('ping', yep).emit('ping',done)
-  })
+  it('should emit ping event and invoke callback', done => ev.on('ping', yep).emit('ping',done))
 
-  it('should invoke in context', function(done) {
+  it('should invoke in context', done => {
     let cx = {}
     ev.on('ping', function() {
-      assert.equal(cx, this)
-      done()
-    }, cx).emit('ping')
+      assert.equal(cx, this);
+      done();
+    }, cx).emit('ping');
+  });
+
+  it('should invoke none',                         () => ev.on('ping', noop).on('ping', noop).on('pong', noop).on('pong', noop).off().emit('ping').emit('pong'));
+  it('should invoke all but specified callback', done => ev.on('ping', yep).on('ping', noop).off('ping', noop).emit('ping', done));
+  it('should invoke all but specified event',    done => ev.on('pong', yep).on('ping', noop).on('ping', noop).off('ping').emit('ping').emit('pong', done));
+})
+
+describe('Vent:once', () => {
+  let ev = vent({})
+  it('should invoke once', () => {
+    let i = 0
+    ev.once('ping', () => i +=1 ).emit('ping').emit('ping');
+    assert.equal(i, 1)
   })
 
-  it('should invoke none', function() {
-    ev.on('ping', noop)
-      .on('ping', noop)
-      .on('pong', noop)
-      .on('pong', noop)
-    .off()
-      .emit('ping')
-      .emit('pong')
-  })
-
-  it('should invoke all but specified callback', function(done) {
-    ev.on('ping', yep)
-      .on('ping', noop)
-    .off('ping', noop)
-      .emit('ping', done)
-  })
-
-  it('should invoke all but specified event', function(done) {
-    ev.on('pong', yep)
-      .on('ping', noop)
-      .on('ping', noop)
-    .off('ping')
-      .emit('ping')
-      .emit('pong', done)
+  it('should invoke in context', () => {
+    let cx = {}, i = 0;
+    ev.once('ping', function() {
+      assert.equal(cx, this);
+      i +=1
+    }, cx).emit('ping').emit('ping');
+    assert.equal(i, 1)
   })
 })
 
-describe('once', () => {
 
-  let ev = new Emitter
-
-  it('should invoke once', function(){
-
-    let i = 0
-
-    ev.once('ping', () => i +=1 )
-      .emit('ping')
-      .emit('ping');
-    assert.equal(i, 1)
+describe('Vent:on multi', () => {
+  let ev = vent({}), expected = 0;
+  it('should add multiple events', () => {
+    ev.on('ping pong', () => expected+=1).emit('ping').emit('pong');
+    assert.equal(expected, 2);
   })
+})
 
-  it('should invoke in context', function() {
-    let cx = {}
-    let i = 0
-    ev.once('ping', function() {
-      assert.equal(cx, this)
-      i +=1
-    }, cx)
-      .emit('ping')
-      .emit('ping');
-
-    assert.equal(i, 1)
+describe('Vent:off multi', () => {
+  let ev = vent({}), expected = 0;
+  it('should add multiple events', () => {
+    ev.on('ping pong bong', () => expected+=1).emit('ping').emit('pong');
+    assert.equal(expected, 2);
+    ev.off('ping pong').emit('ping').emit('pong').emit('bong');
+    assert.equal(expected, 3)
   })
 })
--- vent.js
+++ vent.js
@@ -1,39 +1,49 @@
 'use strict';
+module.exports = vent;
+function vent(obj, $ = Object.create(null)) {
 
-let $ = Symbol('events');
+  return define(obj,
 
-module.exports = class Vent {
-  constructor(a,b) {
-    this[$]=Object.create(null);
+      function on(e, cb, ctx) {
+          e.match(/\S+/g).forEach(x => ($[x]||($[x]=[])).push({ cb, ctx }))
+          return this;
+        },
+
+    function once(e, cb, ctx) {
+        let fn = (e, ...argv) => { cb.apply(ctx, argv); this.off(e, fn); }
+        return this.on(e, fn, this);
+      },
+
+    function emit(e, ...argv) {
+        $[e] && $[e].forEach(x => x.cb.apply(x.ctx, argv));
+        return this;
+      },
+
+    function off(e, cb, arr) {
+        if(e) {
+          if('function'==typeof e)
+            arr = Object.keys($), cb = e;
+          else
+            arr = e.match(/\S+/g);
+          arr.forEach(cb ? x => x in $ && remove($[x], o => cb===o.cb) : x => delete $[x]);
+        } else {
+          $ = Object.create(null);
+        }
+        return this
+      })
   }
 
-  on(e, cb, ctx) {
-      (this[$][e]||(this[$][e]=[])).push({ cb, ctx })
-      return this;
-    }
 
-  once(e, cb, ctx) {
-      let fn = (e, ...argv) => {
-        cb.apply(ctx, argv);
-        this.off(e, fn);
-      }
-      return this.on(e, fn, this);
-    }
-
-  emit(e, ...argv) {
-    let vents = this[$][e];
-    vents && vents.forEach(x => x.cb.apply(x.ctx, argv));
-    return this;
+function remove(list, cb, ctx) {
+    let buf = [], i = -1;
+    while (++i < list.length) {
+      if (cb.call(ctx, list[i], i, list)) {
+        buf.push(list.splice(i, 1)[0]), i--;
+      }}
+    return buf;
   }
 
-  off(e, cb, i=-1, x, vents) {
-    if(!e)
-      this[$] = Object.create(null);
-    else if(!cb)
-      delete this[$][e];
-    else if(vents = this[$][e])
-      while(x = vents[++i])
-        cb === x.cb && vents.splice(i, 1);
-    return this;
-  }
+function define(obj, ...argv) {
+  argv.forEach(value => Object.defineProperty(obj, value.name, { value, configurable: !0, writable: !0}));
+  return obj;
 }
